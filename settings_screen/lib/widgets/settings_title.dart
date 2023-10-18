ae936e8931475d6feb74d57a59094c2198ac0ce5
Ha Manh Do <hadm.haui@gmail.com>
2023-10-19 00:58:41 +0700

    Settings Screen app

---
+++ settings_screen/lib/widgets/settings_title.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+
+class SettingsTile extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  final String title;
+  final VoidCallback onTap;
+
+  const SettingsTile(
+      {super.key,
+      required this.color,
+      required this.icon,
+      required this.title,
+      required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20), color: color),
+        child: Icon(icon, color: Colors.white),
+      ),
+      const SizedBox(width: 10),
+      Text(title,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+      const Spacer(),
+      InkWell(
+        onTap: onTap,
+        child: Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.circular(15)),
+            child: const Icon(Ionicons.chevron_forward_outline,
+                color: Colors.black)),
+      )
+    ]);
+  }
+}
