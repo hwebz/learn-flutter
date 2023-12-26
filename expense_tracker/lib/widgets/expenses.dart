69a73918840fd4a8c24a7fb8b5e11f4a998b6a1e
Ha Manh Do <hadm.haui@gmail.com>
2023-12-27 01:02:02 +0700

    Expense Tracker initialization

---
+++ expense_tracker/lib/widgets/expenses.dart
@@ -0,0 +1,53 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 15.69,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
+  _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context,
+        builder: (ctx) {
+          return Text('Add Expense');
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
+          ],
+          title: Text('Expenses'),
+        ),
+        body: Column(
+          children: [
+            Text('The Chart'),
+            Expanded(child: ExpensesList(expenses: _registeredExpenses))
+          ],
+        ));
+  }
+}
