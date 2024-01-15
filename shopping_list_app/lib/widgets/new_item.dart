00b6ecf65e30d344255c1bced950d0aa618c1860
Ha Manh Do <hadm.haui@gmail.com>
2024-01-15 17:57:11 +0700

    Make BE request by using HTTP package

--- shopping_list_app/lib/widgets/new_item.dart
+++ shopping_list_app/lib/widgets/new_item.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/category.dart';
 import 'package:shopping_list_app/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -23,6 +26,19 @@ class _NewItemState extends State<NewItem> {
       print(_enteredName);
       print(_enteredQuantity);
       print(_selectedCategory);
+
+      final url = Uri.https(
+          'shoppinglistflutter-70bb4-default-rtdb.asia-southeast1.firebasedatabase.app',
+          'shopping_list.json');
+      http.post(url,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: json.encode({
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory!.title
+          }));
 
       Navigator.of(context).pop(GroceryItem(
           id: DateTime.now().toString(),
