2167fa41e576b1ce650e12a85083084620469a98
Ha Manh Do <hadm.haui@gmail.com>
2024-01-25 14:51:23 +0700

    User logout

--- flutter_chat/lib/main.dart
+++ flutter_chat/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/screens/auth.dart';
+import 'package:flutter_chat/screens/chat.dart';
+import 'package:flutter_chat/screens/splash.dart';
 
 import 'firebase_options.dart';
 
@@ -23,6 +26,16 @@ class MyApp extends StatelessWidget {
             useMaterial3: true,
             colorScheme: ColorScheme.fromSeed(
                 seedColor: const Color.fromARGB(255, 63, 17, 177))),
-        home: const AuthScreen());
+        home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (ctx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const SplashScreen();
+              }
+              if (snapshot.hasData) {
+                return const ChatScreen();
+              }
+              return const AuthScreen();
+            }));
   }
 }
